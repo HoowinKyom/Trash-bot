2052b5bbac114ea613f6be5e81376bcb83f65fd3
HoowinKyoma <155293427+HoowinKyom@users.noreply.github.com>
2024-01-06 12:00:58 +0100

    responses.py

--- responses.py
+++ responses.py
@@ -1,29 +1,18 @@
-import random
-
 def get_response(message: str) -> str:
     p_message = message.lower()
 
-    if p_message == 'Bot':
-        return 'Hey there! You called? 🤖 I\'m AssistifyBot, your virtual companion on this Discord adventure. Need assistance, have a question, or just looking for some fun commands? Type !help to discover all the cool things I can do! 🌟'
+    responses = {
+        'bot': 'Hey there! You called? 🤖 I\'m EasyBot, your virtual companion on this Discord adventure. Need assistance, have a question, or just looking for some fun commands? Type !help to discover all the cool things I can do! 🌟',
+        'hello': 'Hey there! 😁',
+        '!help': 'Sure thing! Please make sure to check out the #rules channel for important guidelines. Happy chatting! 📜',
+        'nigga': 'wash your mouth 🫣',
+        'bitch': "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please 🤬.",
+        '!vip': 'https://www.roblox.com/games/12355337193?privateServerLinkCode=55769532370951510012163242173675',
+        'nigger': "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please 🤬.",
+        'nig': "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please 🤬.",
+        'nigg': "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please 🤬.",
+        'nigged': "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please 🤬.",
+        'school': "Fuck school 🔥"
+    }
 
-    if p_message == 'hello':
-        return 'Hey there!'
-
-    if 'bot' in p_message:
-        return 'Hey there! You called? 🤖 I\'m AssistifyBot, your virtual companion on this Discord adventure. Need assistance, have a question, or just looking for some fun commands? Type !help to discover all the cool things I can do! 🌟'
-
-    if p_message == '!help':
-        return 'Sure thing! Please make sure to check out the #rules channel for important guidelines. Happy chatting! 📜'
-    
-    if p_message == 'nigga':
-        return 'wash your mouth 🫣'
-    
-    if p_message == 'bitch':
-        return "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please."
-    
-    if p_message == '!vip':
-        return 'https://www.roblox.com/games/12355337193?privateServerLinkCode=55769532370951510012163242173675'
-    
-    if p_message == 'Nigger' or p_message == 'nigger':
-        return "I'm here to assist and keep things friendly. Let's keep the conversation respectful, please."
-
+    return responses.get(p_message, "I'm sorry, I didn't understand that. Could you please rephrase or type !help for assistance?")
