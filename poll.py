1f548012969fe8bea89235c727c97a27f74d7aac
HoowinKyoma <155293427+HoowinKyom@users.noreply.github.com>
2024-01-06 14:02:11 +0100

    poll.py

--- poll.py
+++ poll.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import tasks
 import datetime as dt
-DISCORD_TOKEN = "MTE5Mjk3NTcwMjUzOTEwODQ4Mw.G5tPDc.-plbwc5cJ_GgSQP5xAosxZU0msAIqEEXKlhmfY"
+DISCORD_TOKEN = "" #add your bot token
 
 
 
@@ -20,7 +20,6 @@ SENT_MESSAGE_IDS = []
 async def on_message(message):
     global POLL_OPTION_EMOJIS
     if message.content.startswith("!create_poll"):
-        # Extract the parameters from the command
         params = message.content.split(";")
         name = params[0].replace("!create_poll","").strip()
         question = params[1].strip()
@@ -34,17 +33,14 @@ async def on_message(message):
         except Exception as e:
             pass
         
-
-        # validate parameters to check if there are any errors
+s
         error = validate_params(name, question, options, countdown)
 
         if error is not None:
-            # If parameters are not in the expected format, send error message
             embed = discord.Embed(title="Error", description=error, color=discord.Color.red())
             sent = await message.channel.send(embed=embed)
             return
 
-        # If there is no error, send the poll message
         for i in range(len(options)):
                 options[i] = f"{POLL_OPTION_EMOJIS[i]} {options[i]}"
         options = '\n'.join(options)
@@ -54,23 +50,17 @@ async def on_message(message):
 
         POLL_OPTION_EMOJIS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"]
         for i in range(options_count):
-            # React with the allowed set of emojis
             await sent.add_reaction(POLL_OPTION_EMOJIS[i])
         
-        # Add sent message id to a global list
         SENT_MESSAGE_IDS.append(sent.id)
         end_time = dt.datetime.utcnow() + dt.timedelta(seconds=int(countdown)*60)
-        # define the background task to update the countdown message
         @tasks.loop(seconds=1)
         async def update_countdown():
-            # Calculate remaining time in countdown
             remaining_time = (end_time - dt.datetime.utcnow()).total_seconds()
 
             if remaining_time > 0:
-                # If countdown still didn't expire
                 minutes, seconds = divmod(int(remaining_time), 60)
 
-                # Edit the message
                 description = f"**{question}**\n{options}\n\n*Poll ends in {minutes:02d}:{seconds:02d}*"
                 embed = discord.Embed(title=f"POLL: {name}", description=description, color=0x12ff51)
                 await sent.edit(embed=embed)
@@ -81,30 +71,22 @@ async def on_message(message):
                 poll_results_count = {}
                 total_reactions = 0
 
-                # If countdown expired
                 for reaction in sent_message.reactions:
-                    # Enumerate message reactions
                     for ind, emoji in enumerate(POLL_OPTION_EMOJIS):
-                        # Count number of times an emoji is reacted
                         if reaction.emoji == emoji:
                             poll_results_count[ind+1] = reaction.count - 1
                             if reaction.count>1:
-                                # ALso calculate the total reactions
                                 total_reactions+=1
-
-                # Craft the results message
+    
                 poll_results_message = ""
-                for ind, count in enumerate(poll_results_count):
-                    # Calculate percentage value of each option                  
+                for ind, count in enumerate(poll_results_count):                 
                     perc = round(poll_results_count[ind+1]/total_reactions * 100)
                     poll_results_message+=f"{orig_options[ind]} ~ {perc}% ({poll_results_count[ind+1]} votes)\n"
                 
-                # Send the results message
                 embed = discord.Embed(title=f"POLL RESULTS: {name}", description=poll_results_message, color=0x13a6f0)
                 await message.channel.send(embed=embed)
 
 
-                # Delete the original poll message and end tasks.loop function
                 await sent_message.delete()
                 update_countdown.cancel()
         
@@ -114,7 +96,6 @@ async def on_message(message):
     @client.event
     async def on_raw_reaction_add(payload):
         global SENT_MESSAGE_IDS
-        # Get the message object
         channel = await client.fetch_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
 
@@ -127,21 +108,16 @@ async def on_message(message):
 
         sent_by_bot = False
         for i in SENT_MESSAGE_IDS:
-            # Compare message ids
             if i==message.id:
                 sent_by_bot = True
                 break
         if not sent_by_bot:
-            # IF not sent by bot, ignore
             return
         
-        # Check if reaction made is allowed
         if payload.emoji.name not in POLL_OPTION_EMOJIS:
-            # Remove reaction
             await message.remove_reaction(payload.emoji.name, member)
             return
 
-        # Remove duplicate votes of the user
         user_reaction_count = 0
         for r in message.reactions:
             async for u in r.users():
